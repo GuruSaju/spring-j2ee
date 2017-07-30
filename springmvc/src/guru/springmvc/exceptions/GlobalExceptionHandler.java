74d4df19fcb7b6fc8fbda73f1a3158e1de56592c
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2017-07-29 20:40:47 -0400

    "no change"

--- springmvc/src/guru/springmvc/exceptions/GlobalExceptionHandler.java
+++ springmvc/src/guru/springmvc/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package guru.springmvc.controller;
+package guru.springmvc.exceptions;
 
 import java.sql.SQLException;
 
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import guru.springmvc.exceptions.ExceptionForGlobal;
 
 /**
  * 
