d04c03c437e6f5d4f0be60f47ecbb1b1d1874ed8
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2017-08-30 22:15:26 -0400

    "spring web configuration"

---
+++ spring-pagination/src/guru/spring/pagination/controllernconfiguration/SpringWebConfig.java
@@ -0,0 +1,36 @@
+package guru.spring.pagination.controllernconfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * 
+ * @author Srisarguru
+ *
+ */
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "guru.spring.pagination" })
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Bean
+	public ViewResolver jspViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/jsps/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
